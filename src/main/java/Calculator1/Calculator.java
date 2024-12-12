e81e081e150aa99743379a13e7aa2913dbb5b3d8
Khawla Barkan <khawlabarkan354@gmail.com>
2024-12-12 09:09:53 +0100

    commit final

--- src/main/java/Calculator1/Calculator.java
+++ src/main/java/Calculator1/Calculator.java
@@ -1,8 +1,14 @@
 package Calculator1;
 
 public class Calculator {
-    public int add(int a, int b) {
-        return a + b;
-
+        public int addi(int a, int b) {
+            return a + b;
+        }
+    public int add(int opG, int opD) {
+        return opG + opD;
     }
+    public int divide(int opG, int opD) {
+        return opG / opD;
+    }
+
 }
--- src/test/java/Calculator1/CalculatorTest.java
+++ src/test/java/Calculator1/CalculatorTest.java
@@ -1,17 +1,26 @@
 package Calculator1;
 
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 class CalculatorTest {
     @Test
     public void testAddition() {
         Calculator calculator = new Calculator();
-        int result = calculator.add(2, 3);
-        // Correction ici : la somme de 2 + 3 est égale à 5
-        assertEquals(6, result, "2 + 3 doit être égal à 5");
+        int result = calculator.addi(2, 2);
+        assertThat(result).isEqualTo(4);;
     }
-
-
+    @Test
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(2, 3);
+        // Vérification avec AssertJ
+        assertThat(result).isEqualTo(7);  // Correction : 2 + 3 doit être égal à 5
+    }
+    @Test
+    void testDivide() {
+        Calculator calculator = new Calculator();
+        int res = calculator.divide(10,2);
+        assertThat(res).isEqualTo(8);
+    }
 }
